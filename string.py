79bd402c5cad9f3f27fe17b0ee169d7df8af395d
Largo3 <98081483+Largo3@users.noreply.github.com>
2024-12-04 12:03:22 +0530

    new

---
+++ string.py
@@ -0,0 +1,28 @@
+
+
+class car:
+    color:str
+
+    shape:str
+
+    varient:str
+
+    brand:str
+
+    def start():
+        pass
+    def accilerate():
+        pass
+
+    def stop():
+
+
+
+def baleno_car=car()
+
+
+
+
+
+
+
---
+++ strings.py
@@ -0,0 +1,42 @@
+
+
+# vovels
+
+# text="hello python"
+
+
+
+
+# text="python java javascript krishna"
+
+
+# # string.split
+# # splits using coma or any such attributes
+
+
+# words=text.split(" ")
+
+# print(words)
+
+
+
+# remove
+
+# text1="\t gokul krishna \t arathi bluu \t"
+
+# text2=text1.lstrip("\t")
+
+# # remove \g
+
+
+# print(text2)
+
+
+
+
+text="gokul krishna is a gooood boy"
+
+# text2=text.replace("o","h")
+# print(text2)
+
+
